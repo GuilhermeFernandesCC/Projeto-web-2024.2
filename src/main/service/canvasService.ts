9b8f3bc70de50a5f07bb43c46d315429d957773a
GuilhermeFernandesCC <guilherme.fernandes@ccc.ufcg.edu.br>
2025-02-21 12:21:16 -0300

    add: Canvas Service

---
+++ src/main/service/canvasService.ts
@@ -0,0 +1,30 @@
+import { CanvasAddDto } from "../Dto/canvasAddDto";
+import { CanvasDto } from "../Dto/canvasDto";
+import { CanvasNotFound } from "../error/canvasNotFound";
+import { canvasAddRepository, canvasDeleteRepository, canvasGetRepository, canvasUpdateRepository } from "../repository/canvasRepository";
+import { tableGetService } from "./tableService";
+
+export const canvasAddService = async (canvasAddDto:CanvasAddDto): Promise<CanvasDto | null> =>{
+    await tableGetService(canvasAddDto.tableId);
+
+    const result = await canvasAddRepository(canvasAddDto);
+    return result as CanvasDto;
+}
+export const canvasGetService = async(id: number): Promise<CanvasDto| null> => {
+    const result = await canvasGetRepository(id);
+    if (result == null){
+        throw CanvasNotFound()
+    }
+
+    return result ;
+};
+export const canvasDeleteService = async(id:number):Promise<CanvasDto|null> => {
+    await canvasGetService(id);
+    const result = await canvasDeleteRepository(id);
+    return result;
+}
+export const canvasUpdateService = async(id: number,canvasAddDto: CanvasAddDto): Promise<CanvasDto| null> => {
+    await canvasGetService(id)
+    const result = await canvasUpdateRepository(id,canvasAddDto)
+    return result;
+};
